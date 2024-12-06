d208c25b0ac8b1d574f65574f195f0bd1f2087ee
林志鹏 <52520207@qq.com>
2024-12-06 11:16:59 +0800

    docs: create table SQL change hump

--- sql/create_table.sql
+++ sql/create_table.sql
@@ -12,24 +12,24 @@ use leveling_artifact;
 create table if not exists user
 (
     id            bigint auto_increment comment 'id' primary key,
-    userAccount   varchar(256)                           not null comment '账号',
-    userPassword  varchar(512)                           not null comment '密码',
-    unionId       varchar(256)                           null comment '微信开放平台id',
-    mpOpenId      varchar(256)                           null comment '公众号openId',
-    userName      varchar(256)                           null comment '用户昵称',
-    userAvatar    varchar(1024)                          null comment '用户头像',
-    userProfile   varchar(512)                           null comment '用户简介',
-    userRole      varchar(256) default 'user'            not null comment '用户角色：user/admin/ban',
-    vipExpireTime datetime                               null comment '会员过期时间',
-    vipCode       varchar(128)                           null comment '会员兑换码',
-    vipNumber     bigint                                 null comment '会员编号',
-    shareCode     varchar(20)  DEFAULT NULL COMMENT '分享码',
-    inviteUser    bigint       DEFAULT NULL COMMENT '邀请用户 id',
-    editTime      datetime     default CURRENT_TIMESTAMP not null comment '编辑时间',
-    createTime    datetime     default CURRENT_TIMESTAMP not null comment '创建时间',
-    updateTime    datetime     default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP comment '更新时间',
-    isDelete      tinyint      default 0                 not null comment '是否删除',
-    index idx_unionId (unionId)
+    user_account   varchar(256)                           not null comment '账号',
+    user_password  varchar(512)                           not null comment '密码',
+    union_id       varchar(256)                           null comment '微信开放平台id',
+    mp_open_id      varchar(256)                           null comment '公众号openId',
+    user_name      varchar(256)                           null comment '用户昵称',
+    user_avatar    varchar(1024)                          null comment '用户头像',
+    user_profile   varchar(512)                           null comment '用户简介',
+    user_role      varchar(256) default 'user'            not null comment '用户角色：user/admin/ban',
+    vip_expire_time datetime                               null comment '会员过期时间',
+    vip_code       varchar(128)                           null comment '会员兑换码',
+    vip_number     bigint                                 null comment '会员编号',
+    share_code     varchar(20)  DEFAULT NULL COMMENT '分享码',
+    invite_user    bigint       DEFAULT NULL COMMENT '邀请用户 id',
+    edit_time      datetime     default CURRENT_TIMESTAMP not null comment '编辑时间',
+    create_time    datetime     default CURRENT_TIMESTAMP not null comment '创建时间',
+    update_time    datetime     default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP comment '更新时间',
+    is_delete      tinyint      default 0                 not null comment '是否删除',
+    index idx_unionId (union_id)
 ) comment '用户' collate = utf8mb4_unicode_ci;
 
 -- 题库表
@@ -39,17 +39,17 @@ create table if not exists question_bank
     title         varchar(256)                       null comment '标题',
     description   text                               null comment '描述',
     picture       varchar(2048)                      null comment '图片',
-    userId        bigint                             not null comment '创建用户 id',
-    reviewStatus  int      default 0                 not null comment '状态：0-待审核, 1-通过, 2-拒绝',
-    reviewMessage varchar(512)                       null comment '审核信息',
-    reviewerId    bigint                             null comment '审核人 id',
-    reviewTime    datetime                           null comment '审核时间',
+    user_id        bigint                             not null comment '创建用户 id',
+    review_status  int      default 0                 not null comment '状态：0-待审核, 1-通过, 2-拒绝',
+    review_message varchar(512)                       null comment '审核信息',
+    reviewer_id    bigint                             null comment '审核人 id',
+    review_time    datetime                           null comment '审核时间',
     priority      int      default 0                 not null comment '优先级',
-    viewNum       int      default 0                 not null comment '浏览量',
-    editTime      datetime default CURRENT_TIMESTAMP not null comment '编辑时间',
-    createTime    datetime default CURRENT_TIMESTAMP not null comment '创建时间',
-    updateTime    datetime default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP comment '更新时间',
-    isDelete      tinyint  default 0                 not null comment '是否删除',
+    view_num       int      default 0                 not null comment '浏览量',
+    edit_time      datetime default CURRENT_TIMESTAMP not null comment '编辑时间',
+    create_time    datetime default CURRENT_TIMESTAMP not null comment '创建时间',
+    update_time    datetime default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP comment '更新时间',
+    is_delete      tinyint  default 0                 not null comment '是否删除',
     index idx_title (title)
 ) comment '题库' collate = utf8mb4_unicode_ci;
 
@@ -61,34 +61,34 @@ create table if not exists question
     content       text                               null comment '内容',
     tags          varchar(1024)                      null comment '标签列表（json 数组）',
     answer        text                               null comment '推荐答案',
-    userId        bigint                             not null comment '创建用户 id',
-    reviewStatus  int      default 0                 not null comment '状态：0-待审核, 1-通过, 2-拒绝',
-    reviewMessage varchar(512)                       null comment '审核信息',
-    reviewerId    bigint                             null comment '审核人 id',
-    reviewTime    datetime                           null comment '审核时间',
-    viewNum       int      default 0                 not null comment '浏览量',
-    thumbNum      int      default 0                 not null comment '点赞数',
-    favourNum     int      default 0                 not null comment '收藏数',
+    user_id        bigint                             not null comment '创建用户 id',
+    review_status  int      default 0                 not null comment '状态：0-待审核, 1-通过, 2-拒绝',
+    review_message varchar(512)                       null comment '审核信息',
+    reviewer_id    bigint                             null comment '审核人 id',
+    review_time    datetime                           null comment '审核时间',
+    view_num       int      default 0                 not null comment '浏览量',
+    thumb_num      int      default 0                 not null comment '点赞数',
+    favour_num     int      default 0                 not null comment '收藏数',
     priority      int      default 0                 not null comment '优先级',
-    needVip       tinyint  default 0                 not null comment '仅会员可见（1 表示仅会员可见）',
-    editTime      datetime default CURRENT_TIMESTAMP not null comment '编辑时间',
-    createTime    datetime default CURRENT_TIMESTAMP not null comment '创建时间',
-    updateTime    datetime default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP comment '更新时间',
-    isDelete      tinyint  default 0                 not null comment '是否删除',
+    need_vip       tinyint  default 0                 not null comment '仅会员可见（1 表示仅会员可见）',
+    edit_time      datetime default CURRENT_TIMESTAMP not null comment '编辑时间',
+    create_time    datetime default CURRENT_TIMESTAMP not null comment '创建时间',
+    update_time    datetime default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP comment '更新时间',
+    is_delete      tinyint  default 0                 not null comment '是否删除',
     index idx_title (title),
-    index idx_userId (userId)
+    index idx_userId (user_id)
 ) comment '题目' collate = utf8mb4_unicode_ci;
 
 -- 题库题目表（硬删除）
 create table if not exists question_bank_question
 (
     id             bigint auto_increment comment 'id' primary key,
-    questionBankId bigint                             not null comment '题库 id',
-    questionId     bigint                             not null comment '题目 id',
-    userId         bigint                             not null comment '创建用户 id',
-    createTime     datetime default CURRENT_TIMESTAMP not null comment '创建时间',
-    updateTime     datetime default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP comment '更新时间',
-    UNIQUE (questionBankId, questionId)
+    question_bank_id bigint                             not null comment '题库 id',
+    question_id     bigint                             not null comment '题目 id',
+    user_id         bigint                             not null comment '创建用户 id',
+    create_time     datetime default CURRENT_TIMESTAMP not null comment '创建时间',
+    update_time     datetime default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP comment '更新时间',
+    UNIQUE (question_bank_id, question_id)
 ) comment '题库题目' collate = utf8mb4_unicode_ci;
 
 
